f816af982b1afe10a14d6d3b6c63308b1ed1e1dc
Ray Madachy <madachy@gmail.com>
2024-05-02 19:09:10 -0700

    Update __init__.py

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,3 +1,4 @@
 from .simulation import init_sd_model, build_model, add_stock, add_auxiliary, add_flow, plot_graph, save_graph, run_model, set_logical_run_time, get_logical_end_time, draw_sd_model, draw_model_diagram, init_de_model, add_source, add_server, add_delay, add_terminate, run_de_model, draw_discrete_model_diagram, plot_histogram
 from .cost_models import cosysmo, phase_effort
+from .diagrams import context_diagram, activity_diagram, use_case_diagram, sequence_diagram, design_structure_matrix, design_structure_matrix, tree, fault_tree_diagram, read_fault_tree_excel, critical_path_diagram, draw_fault_tree_diagram_quantitative, fault_tree_cutsets 
 
