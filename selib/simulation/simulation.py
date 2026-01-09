1ce2053531d4ac1f845a85223cccd990994e46dc
Ray Madachy <madachy@gmail.com>
2026-01-08 17:57:56 -0800

    Update simulation.py

--- selib/simulation/simulation.py
+++ selib/simulation/simulation.py
@@ -624,7 +624,7 @@ class SystemDynamicsModel:
 
         graph = graphviz.Digraph(engine='dot', filename=filename, format=format)
         graph.attr("graph", pad="1")
-        graph.attr(rankdir='LR', size='10,8', splines='spline', margin="1.5,0")
+        graph.attr(rankdir='LR', splines='spline', margin="1.5,0") #size='10,8'
         graph.attr('node', fontname="arial", fontcolor='blue', color='invis', fontsize='10')
 
         with graph.subgraph(name='cluster_flowchain') as c:
